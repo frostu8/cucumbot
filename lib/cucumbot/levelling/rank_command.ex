b698be9bf8e6c6439c2aed11e43a0a15e81c5d64
Dante <frostu8@protonmail.com>
2021-03-04 12:36:46 -0500

    fix rank command

--- lib/cucumbot/levelling/rank_command.ex
+++ lib/cucumbot/levelling/rank_command.ex
@@ -3,16 +3,43 @@ defmodule Cucumbot.Levelling.RankCommand do
 
   alias Cucumbot.Command.Arguments, as: CommandArgs
   alias Cucumbot.Levelling.Store
+  alias Cucumbot.Util
 
   alias Nostrum.Api
-  alias Nostrum.Cache.UserCache
 
   def execute(args, msg) do
-    user_id = case CommandArgs.next_arg(args) do
-      {nil, args} ->
+    {user_mention, _args} = CommandArgs.next_arg(args)
+
+    user_id = case user_mention do
+      nil ->
         msg.author.id
-      {user_mention, args} ->
-        Cucumbot.Util.User.resolve_mention(user_mention)
+      user_mention ->
+        Util.User.resolve_mention(user_mention)
+    end
+
+    case user_id do
+      nil ->
+        # could not resolve mention
+        Api.create_message(msg.channel_id,
+          content: "could not find user #{user_mention}",
+          allowed_mentions: :none)
+      user_id ->
+        # get member
+        case Util.User.get_member(msg.guild_id, user_id) do
+          {:error, why} ->
+            # could not find user
+            Api.create_message(msg.channel_id,
+              content: "could not find user #{user_mention}",
+              allowed_mentions: :none)
+          {:ok, member} ->
+            # got member
+            score = Store.get_or_default(user_id, msg.guild_id).score
+
+            Api.create_message(msg.channel_id,
+              "member \"#{member.nick || member.user.username}\" is " <> 
+                "lv#{Cucumbot.Levelling.score_to_level(score)} " <> 
+                  "with #{score} score")
+        end
     end
   end
 end
--- lib/cucumbot/util/user.ex
+++ lib/cucumbot/util/user.ex
@@ -1,4 +1,12 @@
 defmodule Cucumbot.Util.User do
+  alias Nostrum.Cache.GuildCache
+
+  alias Nostrum.Struct.Guild
+  alias Nostrum.Struct.Guild.Member
+  alias Nostrum.Struct.User
+
+  alias Nostrum.Api
+
   @doc """
   Attempts to resolve a mention into a user ID.
   """
@@ -17,6 +25,34 @@ defmodule Cucumbot.Util.User do
         nil
       {id, _str} ->
         id
+    end
+  end
+
+  @doc """
+  Attempts to get a fully qualified guild member from the API and the cache.
+  """
+  @spec get_member(Guild.id, User.id) :: {:ok, Member.t} | {:error, String.t}
+  def get_member(guild_id, user_id) do
+    case GuildCache.get(guild_id) do
+      {:ok, guild} ->
+        case Map.get(guild.members, user_id) do
+          nil ->
+            get_member_api(guild_id, user_id)
+          member ->
+            {:ok, member}
+        end
+      {:error, _reason} ->
+        get_member_api(guild_id, user_id)
+    end
+  end
+
+  defp get_member_api(guild_id, user_id) do
+    case Api.get_guild_member(guild_id, user_id) do
+      {:ok, member} ->
+        {:ok, member}
+      {:error, _why} ->
+        {:error, "guild is not in cache, and no member with ID" <>
+          "#{user_id} could be found"}
     end
   end
 end
